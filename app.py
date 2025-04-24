94505bc9c0e38da7cd88968b3766cf6135274839
starksindustries <Siseho.Chibeya@upsvr.gov.sk>
2025-04-24 12:31:30 +0000

    connecting to currents api 2

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import requests
 from flask import Flask, jsonify
+import os
 
 app = Flask(__name__)
 
-CURRENTS_API_KEY = "X3Wbtk3d0sWsOPbMI9iG3Ye_3oZ7zUuvbNV1azolS4lPtWXv"
+CURRENTS_API_KEY = os.getenv("CURRENTS_API_KEY")
+if not CURRENTS_API_KEY:
+    return jsonify({"error": "❌ CURRENTS_API_KEY not set"}), 500
 SEARCH_QUERY = "belt and road"
 API_ENDPOINT = f"https://api.currentsapi.services/v1/search?keywords={SEARCH_QUERY}&language=en"
 
@@ -23,7 +26,7 @@ def get_news():
 
     if "news" not in data or not data["news"]:
         print("❗ No articles found.")
-        return jsonify({"message": f"❗ No articles found mentioning '{SEARCH_QUERY}'."})
+        return jsonify({"message": f"❗ No articles found mentioning '{SEARCH_QUERY}' AGAIN!"})
 
     print(f"✅ Found {len(data['news'])} articles mentioning '{SEARCH_QUERY}'.")
 
