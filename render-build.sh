8534ef5446973a0c05fdb86e8ac8aca48572098a
starksindustries <Siseho.Chibeya@upsvr.gov.sk>
2025-04-29 11:02:04 +0000

    docker chrome selenium 17

--- render-build.sh
+++ render-build.sh
@@ -6,10 +6,15 @@ echo "📦 Installing Chrome and Chromedriver..."
 mkdir -p .render/chrome
 
 # Download prebuilt Chromium + Chromedriver bundle
-curl -SL https://github.com/BugzTheBunny/chrome-on-render/releases/latest/download/chrome-on-render.tar.gz | tar -xzC .render/chrome
+curl -SL https://storage.googleapis.com/chrome-for-testing-public/135.0.7049.114/linux64/chrome-linux64.zip -o .render/chrome/chrome-linux64.zip
+curl -SL https://storage.googleapis.com/chrome-for-testing-public/135.0.7049.114/linux64/chromedriver-linux64.zip -o .render/chrome/chromedriver-linux64.zip
+
+# Unzip the downloaded files
+unzip .render/chrome/chrome-linux64.zip -d .render/chrome/
+unzip .render/chrome/chromedriver-linux64.zip -d .render/chrome/
 
 # Make Chrome binary executable
-chmod +x .render/chrome/opt/google/chrome/google-chrome
+chmod +x .render/chrome/chrome-linux64/chrome
 
 # Print structure for verification
 echo "📂 Installed files:"
